ee838364eee17ddc07b79e7918bb9042b106f265
Thiago Meireles <thiago22md@gmail.com>
2023-12-07 18:10:07 -0300

    script_base

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTag = document.querySelectorAll("select");
+const icons = document.querySelectorAll(".row i");
+const translateBtn = document.querySelector("buttom");
+
+selectTag.forEach((tag, id) => {
+    
+});
+
+exchangeIcon.addEventListener("click", () => {
+
+});
+
+fromText.addEventListener("keyup", () => {
+
+});
+
+translateBtn.addEventListener("click", () => {
+
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+
+    });
+});
