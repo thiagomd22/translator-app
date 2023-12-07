f31962d74742ca5b626080c80c15cfa2eb036b2c
Thiago Meireles <thiago22md@gmail.com>
2023-12-07 18:14:39 -0300

    selectTag

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@ const icons = document.querySelectorAll(".row i");
 const translateBtn = document.querySelector("buttom");
 
 selectTag.forEach((tag, id) => {
-    
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "de-DE" ? "selected" : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
 });
 
 exchangeIcon.addEventListener("click", () => {
